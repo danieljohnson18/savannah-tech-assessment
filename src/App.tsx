aacff25b7d18b022a18c5e45686de747f96507b5
Daniel Arikawe <arikawedaniel@gmail.com>
2024-03-19 21:34:39 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { SideTab } from './component'
+
+function App() {
+  return (
+     <div>
+      <SideTab/>
+     </div>
+  )
+}
+
+export default App
---
+++ src/component/side-tab.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideTab = () => {
+  return (
+    <div>SideTab</div>
+  )
+}
+
+export default SideTab
